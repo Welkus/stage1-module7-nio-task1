e9e80348295308e8aaf1f4325cd2c103359c08c6
Welkus <vdelkus@gmail.com>
2023-11-07 01:48:04 +0200

    Gucci

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,33 @@
 package com.epam.mjc.nio;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        String name;
+        int age;
+        String email;
+        long phone;
+
+        try (java.io.FileReader fileReader = new java.io.FileReader(file);
+             BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+
+            name = bufferedReader.readLine().split(": ")[1];
+            age = Integer.parseInt(bufferedReader.readLine().split(": ")[1]);
+            email = bufferedReader.readLine().split(": ")[1];
+            phone = Long.parseLong(bufferedReader.readLine().split(": ")[1]);
+
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return new Profile(name, age, email, phone);
     }
 }
